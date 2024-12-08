4e08bb0f643b4e104d3f56d8e9edd01fc6508db6
busymilk <1061017244@qq.com>
2024-12-08 20:32:45 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -4,21 +4,21 @@ set -e
 DATE=`date '+%T'`
 
 echo "$DATE: Getting data updates...1"
-curl -o "/tmp/default.upstream" https://raw.githubusercontent.com/busymilk/AdguardHomeSetting/refs/heads/master/dns_for_default > /dev/null 2>&1
+curl -o "/tmp/default.upstream" https://cdn.jsdelivr.net/busymilk/AdguardHomeSetting/refs/heads/master/dns_for_default > /dev/null 2>&1
 
 echo "$DATE: Getting data updates...1.5"
-curl -o "/tmp/special.upstream" https://raw.githubusercontent.com/busymilk/AdguardHomeSetting/refs/heads/master/dns_for_special > /dev/null 2>&1
+curl -o "/tmp/special.upstream" https://cdn.jsdelivr.net/busymilk/AdguardHomeSetting/refs/heads/master/dns_for_special > /dev/null 2>&1
 
 
 echo "$DATE: Getting data updates...2"
-curl -o /tmp/chinalist.upstream https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/refs/heads/master/accelerated-domains.china.conf
+curl -o /tmp/chinalist.upstream https://cdn.jsdelivr.net/felixonmars/dnsmasq-china-list/refs/heads/master/accelerated-domains.china.conf
 
 echo "$DATE: Processing data format...3"
 cat "/tmp/default.upstream" "/tmp/special.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
 sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
 sed -i  "s|/114|/]114|g" /usr/share/adguardhome.upstream
 
-response=$(curl -s https://raw.githubusercontent.com/busymilk/AdguardHomeSetting/refs/heads/master/dns_only_for_china.conf)
+response=$(curl -s https://cdn.jsdelivr.net/busymilk/AdguardHomeSetting/refs/heads/master/dns_only_for_china.conf)
 sed -i "s|114.114.114.114|$response|g" /usr/share/adguardhome.upstream
 
 echo "$DATE: Cleaning...4"
